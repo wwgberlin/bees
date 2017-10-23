12faf6010a09ebff84bd46e13e0f521a4a9a1e85
Ronna Steinberg <ronna.steinberg@daliaresearch.com>
2017-10-23 16:22:04 +0200

    fixed facial detection

--- SkinDetector/face_detector/face_detector.py
+++ SkinDetector/face_detector/face_detector.py
@@ -10,7 +10,8 @@ def process(img):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        cropped = img[x:y, w:h]
+        cropped = img[y:y+h, x:x+w]
+        # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
         return cropped
     return img
 
